686c11e784a39667adb08d52eafef11c63b6327d
Viktor Videnov <victor.valentinov.videnov@gmail.com>
2022-06-15 17:35:30 +0300

    finaly fixes

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -10,6 +10,7 @@ import { toast } from 'react-toastify'
 
 
 function CreateListing() {
+    // eslint-disable-next-line
     const [geoLocationEnabled, setGeoLocationEnabled] = useState(true);
     const [loading, setLoading] = useState(false)
     const [formData, setFormData] = useState({
@@ -101,7 +102,6 @@ function CreateListing() {
         } else {
             geolocation.lat = latitude
             geolocation.lng = longitude
-            location = address
         }
 
         //Gladston 45 Vidin Bulgaria
@@ -120,7 +120,6 @@ function CreateListing() {
                     (snapshot) => {
                         // Observe state change events such as progress, pause, and resume
                         // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-                        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
                         // console.log('Upload is ' + progress + '% done');
                         switch (snapshot.state) {
                             case 'paused':
@@ -129,6 +128,7 @@ function CreateListing() {
                             case 'running':
                                 // console.log('Upload is running');
                                 break;
+                            default: break; 
                         }
                     },
                     (error) => {
@@ -160,15 +160,15 @@ function CreateListing() {
             timestamp: serverTimestamp()
         }
 
+        formData.location = address
         delete formDataCopy.images
         delete formDataCopy.address
-        location && (formDataCopy.location = location)
         !formDataCopy.offer && delete formDataCopy.discountedPrice
 
         const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
         setLoading(false)
         toast.success('Listing saved')
-        navigate(`/category/${formDataCopy.type}/${docRef}`)
+        navigate(`/category/${formDataCopy.type}/${docRef.id}`)
     }
 
     const onMutate = (e) => {
